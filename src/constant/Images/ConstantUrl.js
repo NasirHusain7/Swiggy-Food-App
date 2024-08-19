88b9839e83d00b3031fabfcfba66c18e058ebbf8
NasirHusain7 <nasirhusain8920@gmail.com>
2024-08-19 19:15:07 +0530

    changed API

--- src/constant/Images/ConstantUrl.js
+++ src/constant/Images/ConstantUrl.js
@@ -1,3 +1,7 @@
-export const SWIGGY_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+// export const SWIGGY_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
-export const RESTAURANT_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId="
+// export const RESTAURANT_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId="
+
+export const SWIGGY_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=32.21460&lng=76.31370&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+
+export const RESTAURANT_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=32.21460&lng=76.31370&restaurantId="
